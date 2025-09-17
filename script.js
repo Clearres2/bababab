a75db41b581668b4ff4328a5d7d4bb3bf28d9952
Clearres2 <1deltafordelta2@gmail.com>
2025-09-17 21:45:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2851,7 +2851,7 @@ const deleteBtn2124 = document.createElement('button');
     tracksContainer.style.maxHeight = '0'; 
     tracksContainer.style.overflow = 'hidden';
 
-   const sortedTracks = [...track.album].sort((a, b) => parseDate(b.titleDate) - parseDate(a.titleDate));
+   const sortedTracks = [...track.album].sort((a, b) => parseDate(a.titleDate) - parseDate(b.titleDate));
 
 
     sortedTracks.forEach((albom, albomIndex) => {
